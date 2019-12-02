9cc4db59b9faef212e110cdc7fe651280e61fbfe
Olya <olya@MacBook-Pro-Olya.local>
2019-12-02 17:13:42 +0200

    Updates

---
+++ precache-manifest.226b5734fab28e7cf381e3f39545d0bd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce4aa112673085de93f59a596999a711",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "189c29828b8d6f15a5ce",
+    "url": "/movie-app/static/css/main.6c8c4b1b.chunk.css"
+  },
+  {
+    "revision": "30175cb156f285e3e62d",
+    "url": "/movie-app/static/js/2.f958c4be.chunk.js"
+  },
+  {
+    "revision": "189c29828b8d6f15a5ce",
+    "url": "/movie-app/static/js/main.c58bf40e.chunk.js"
+  },
+  {
+    "revision": "5ccce974657b8a913657",
+    "url": "/movie-app/static/js/runtime-main.1f64301b.js"
+  },
+  {
+    "revision": "8960524641208dd34fd64bbb4f6ef355",
+    "url": "/movie-app/static/media/camera.89605246.svg"
+  },
+  {
+    "revision": "c7ade6ba422e78bc02eaa8cd3a2fb7b4",
+    "url": "/movie-app/static/media/close-cursor.c7ade6ba.svg"
+  },
+  {
+    "revision": "8f2c6207e4b025a6a94ec6a73bcd64a6",
+    "url": "/movie-app/static/media/default-poster.8f2c6207.jpg"
+  },
+  {
+    "revision": "0eeae0c667a25467ca896eb9a865b813",
+    "url": "/movie-app/static/media/icon-font.0eeae0c6.woff"
+  },
+  {
+    "revision": "42493b495a35bf45cf0b69e1255b3cb7",
+    "url": "/movie-app/static/media/icon-font.42493b49.ttf"
+  },
+  {
+    "revision": "92cd4c4535a4ce302396b67c393b35df",
+    "url": "/movie-app/static/media/icon-font.92cd4c45.woff2"
+  },
+  {
+    "revision": "b2125af3f3a5cf9583238fb0f67c1131",
+    "url": "/movie-app/static/media/icon-font.b2125af3.eot"
+  },
+  {
+    "revision": "cd5ff4755de17f26b536df744579b172",
+    "url": "/movie-app/static/media/icon-font.cd5ff475.svg"
+  }
+]);
